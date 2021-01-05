24e22dcd5ad17e1240279cbf83fe346755bf1027
drewbrad4 <drewbrad4@gmail.com>
2021-01-04 22:06:17 -0800

    Converted all class components to functional but the forms and App.js

--- app/javascript/components/Pages/ApplicationIndex.js
+++ app/javascript/components/Pages/ApplicationIndex.js
@@ -3,27 +3,25 @@ import PropTypes from "prop-types"
 import { Link } from 'react-router-dom'
 import { Button } from 'reactstrap'
 
-class ApplicationIndex extends Component {
-  render () {
-    return (
-      <>
-        <h1>Applications</h1>
+const ApplicationIndex = () => {
+  return (
+    <>
+      <h1>Applications</h1>
 
-        { this.props.applications.map((application, index) => {
-          return(
-            <div key={index} className="applicationCard">
-              <h3>{application.position} - {application.company}</h3>
-              <h5>{application.listing}</h5>
-              <p>Status: {application.status}</p>
-              <Link to={`/application/${application.id}`} className="button">
-                  <Button className="info">More Info</Button>
-              </Link>
-            </div>
-          )
-        })}
-      </>
-    );
-  }
+      { this.props.applications.map((application, index) => {
+        return(
+          <div key={index} className="applicationCard">
+            <h3>{application.position} - {application.company}</h3>
+            <h5>{application.listing}</h5>
+            <p>Status: {application.status}</p>
+            <Link to={`/application/${application.id}`} className="button">
+                <Button className="info">More Info</Button>
+            </Link>
+          </div>
+        )
+      })}
+    </>
+  );
 }
 
 export default ApplicationIndex
