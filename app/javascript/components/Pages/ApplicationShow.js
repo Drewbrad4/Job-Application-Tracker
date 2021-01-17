f46f3ba793438a0aee498b6aa8cb9084ef5cabf6
Drew Bradford <drewbrad4@macs-Mac-mini.san.rr.com>
2021-01-16 16:43:51 -0800

    Switched the show page to display table

--- app/javascript/components/Pages/ApplicationShow.js
+++ app/javascript/components/Pages/ApplicationShow.js
@@ -1,25 +1,62 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { Link } from 'react-router-dom'
-import { Button } from 'reactstrap'
+import { Button, Table } from 'reactstrap'
 
 const ApplicationShow = ({application}) => {
   return (
     <div className="page">
       <div className="applicationCard">
-          <h3 className="show-heading">{application.position} - {application.company}</h3>
-          <p className="app-info">Listing: <a href={`http://${application.listing}`}>{application.listing}</a></p>
-          <p className="app-info">Salary: {application.salary}</p>
-          <p className="app-info">Contact: {application.contact} - {application.contact_email}</p>
-          <p className="app-info">Date Applied: {application.date_applied}</p>
-          <p className="app-info">Summary: {application.summary}</p>
-          { application.interview &&
-          <p className="app-info">Interview: {application.interview}</p>}
-          <p className="app-info">Follow Up: {application.follow_up}</p>
-          <p className="app-info">Status: {application.status}</p>
-          { application.notes &&
-          <p className="app-info">Notes: {application.notes}</p>}
-          <Button>
+      {/* className="app-info" className="show-heading" */}
+        <Table>
+          <thead>
+            <tr>
+              <th>Position:</th>
+              <th>{application.position} - {application.company}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Listing:</th>
+              <td><a href={`http://${application.listing}`}>{application.listing}</a></td>
+            </tr>
+            <tr>
+              <th scope="row">Salary:</th>
+              <td>{application.salary}</td>
+            </tr>
+            <tr>
+              <th scope="row">Contact:</th>
+              <td>{application.contact} - {application.contact_email}</td>
+            </tr>
+            <tr>
+              <th scope="row">Date Applied:</th>
+              <td>{application.date_applied}</td>
+            </tr>
+            <tr>
+              <th scope="row">Summary:</th>
+              <td>{application.summary}</td>
+            </tr>
+            {application.interview &&
+              <tr>
+                <th scope="row">Interview:</th>
+                <td>{application.interview}</td>
+              </tr>}
+            <tr>
+              <th scope="row">Follow Up:</th>
+              <td>{application.follow_up}</td>
+            </tr>
+            <tr>
+              <th scope="row">Status:</th>
+              <td>{application.status}</td>
+            </tr>
+            {application.notes &&
+              <tr>
+                <th scope="row">Notes:</th>
+                <td>{application.notes}</td>
+              </tr>}
+          </tbody>
+        </Table>
+        <Button>
               <Link to={`/editapplication/${application.id}`} className="button">
                   Edit Application
               </Link>
